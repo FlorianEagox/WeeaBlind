2b15d2c1450f46f256df227687e3f2c00d666c33
Tessa Painter <tessa@tessapainter.com>
2024-02-05 02:50:16 -0600

    FIXING STUPID ESPEAK BULLSHIT ONCE AND FOR ALL #11, #6, #5 i am so sorry pwease forgive me

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-# This file is just a quick script for whatever I'm testing at the time, it's not really important 
+import espeakng
 
-# testing XTTS / VC models
+mySpeaker = espeakng.Speaker()
 
-from TTS.api import TTS
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to('cuda')
-
-# generate speech by cloning a voice using default settings
-tts.tts_to_file(text="Welcome to DougDoug, where we solve problems that no one has",
-                file_path="/media/tessa/SATA SSD1/AI MODELS/cloning/output/doug.wav",
-                speaker_wav="/media/tessa/SATA SSD1/AI MODELS/cloning/doug.wav",
-                language="en")
+mySpeaker.say('Whats up nigga its ya gurl doom', True, "aaa.wav")
